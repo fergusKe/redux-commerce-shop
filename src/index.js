b3a52cd34060d62b3f4c0de8209fcf331e8a5d89
fergusKe <fergus.jingtai@gmail.com>
2018-01-08 02:21:24 +0800

    7. Preparing data for product page

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import { Provider } from 'react-redux'
 
 import Layout from 'containers/layout'
 import Phones from 'containers/phones'
+import Phone from 'containers/phone'
 
 import reducers from 'reducers'
 
@@ -25,6 +26,7 @@ ReactDOM.render(
 			<Route component={Layout}>
 				<Route path="/" component={Phones} />
 			</Route>
+			<Route path="/phones/:id" component={Phone} />
 		</Router>
 	</Provider>,
 	document.getElementById('root')
